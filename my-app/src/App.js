22f343c01f606ea39389d9083c38067527c07e00
¨Jane <smoreira11@hotmail.com>
2024-12-03 22:18:20 +0900

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Usuario } from './components/Usuario';
+function App() {
+ 
+  return (
+    <div>
+      <Usuario/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Usuario.js
@@ -0,0 +1,10 @@
+
+
+
+export  const Usuario = () => {
+    return <div>
+       <h1>nombre : seba  </h1>
+       <p> edad : 42  </p>
+       <p> nacionalidad : Chileno  </p>
+    </div>  
+}
