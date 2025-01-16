a92ac155fd01200d0708f9e59cfc7865ce902e7d
¨Jane <smoreira11@hotmail.com>
2025-01-16 22:21:10 +0900

    add new

---
+++ my-app/src/components/machine.jsx
@@ -0,0 +1,13 @@
+import React from "https://esm.sh/react"
+import ReactDom from ""
+import '.*.css';
+function app() {
+  return (
+     <div id="quote-box" >ddddd
+       <a  id="text" > </a>
+       <a  id="author" ></a>
+     </div>
+  )
+}
+
+export default app;
